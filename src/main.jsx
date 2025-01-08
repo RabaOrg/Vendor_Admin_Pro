eb713919a43334247eec3e729dab8cc203eb22fd
Omodolapo20 <122370795+Omodolapo20@users.noreply.github.com>
2025-01-08 23:51:40 +0100

    login authentication

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css"
-
-
-import App from './App.jsx'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes/index.jsx'
 
