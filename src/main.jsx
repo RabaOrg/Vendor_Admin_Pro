ee6260fa51f417ed818765ddd0d5df54397f6678
Omodolapo20 <122370795+Omodolapo20@users.noreply.github.com>
2025-01-09 12:23:04 +0100

    react-toastify

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css"
+import 'react-toastify/dist/ReactToastify.css';
+
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes/index.jsx'
 
